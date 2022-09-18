220e5cdc01205fd6788c8e9820c46368969fa4d1
Okeke Bernard Onyeabor <Onyi700@gmail.com>
2022-09-18 04:02:22 -0700

    first c commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	printf("Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,\n")
+	printf("Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
